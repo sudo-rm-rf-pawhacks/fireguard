b9391ed781e86ab8794e8166e279400243a906a6
Santiordon <santiordon@yahoo.com>
2025-03-16 12:44:53 -0700

    Static Changes 2

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // Ensures static export if you don’t use server functions
-  trailingSlash: true, // Helps with some static route issues
 };
 
 export default nextConfig;
